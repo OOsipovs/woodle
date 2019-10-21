71f867eec0f66e283213b19a1774477fd0edcdc4
Olegs Osipovs <olegs.osipovs@gmail.com>
2019-10-21 21:54:25 +0300

    Index page added

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Welcome to Woodle!</title>
   </head>
   <body>
+    <div class="mainSection">
+      <div class="logoContainer">
+        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Image">
+      </div>
 
+      <div class="searchContainer">
+        <input type="text" name="term" class="searchBox">
+        <input type="submit" value="Search"  class="searchButton">
+      </div>
+
+    </div>
   </body>
 </html>
