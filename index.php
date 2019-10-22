70a5fb78f71bdc263f307b6d1e43d7d50c37c2a3
Olegs Osipovs <olegs.osipovs@gmail.com>
2019-10-22 21:38:49 +0300

    Styles added

--- index.php
+++ index.php
@@ -3,18 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to Woodle!</title>
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <div class="mainSection">
-      <div class="logoContainer">
-        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Image">
-      </div>
 
-      <div class="searchContainer">
-        <input type="text" name="term" class="searchBox">
-        <input type="submit" value="Search"  class="searchButton">
-      </div>
+    <div class="wrapper indexPage">
+      <div class="mainSection">
+        <div class="logoContainer">
+          <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Image">
+        </div>
 
+        <div class="searchContainer">
+          <form action="search.php" method="GET">
+
+            <input type="text" name="term" class="searchBox">
+            <input type="submit" value="Search"  class="searchButton">
+
+
+          </form>
+        </div>
+      </div>
     </div>
+
   </body>
 </html>
